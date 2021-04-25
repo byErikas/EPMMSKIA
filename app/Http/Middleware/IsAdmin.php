edc5c66b1edd31c2211b2bdf0bf13d6189756fca
Erikas <erikas.by8092@go.kauko.lt>
2021-04-25 18:33:11 +0300

    clean up

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -21,6 +21,6 @@ class IsAdmin
             return $next($request);
        }
 
-       return redirect('dashboard')->with('error','You have not admin access');
+       return redirect('dashboard')->with('error','You do not have admin access');
     }
 }
