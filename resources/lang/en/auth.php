174197221f7542854a79d4e4575a2c29f92c836d
Erikas <erikas.by8092@go.kauko.lt>
2021-05-25 19:40:16 +0300

    localization

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Įvesti duomenys yra neteisingi.',
+    'password' => 'Netinkamas slaptažodis.',
+    'throttle' => 'Per daug prisijungimo bandymų. Bandykite vėl po :seconds sekundžių.',
 
 ];
